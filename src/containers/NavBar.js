5436c009f0991911c5f42583cfbc159914f56ff7
nicholasstano <nicholasstano@gmail.com>
2020-02-04 10:32:18 -0500

    small bootstrap edits, trying to figure out the look

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -6,29 +6,29 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-                <div className="navbar">
-                    <Link className="navbar-link" to="/">
-                        <button type="button" class="btn btn-outline-success">onats</button>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <Link className="navbar-brand" to="/">
+                        <button type="button" class="btn btn-outline-dark">onats</button>
                     </Link>
-                    <Link className="navbar-link" to="/movies">
-                        <button type="button" class="btn btn-outline-success">movies</button>
+                    <Link className="navbar-brand" to="/movies">
+                        <button type="button" class="btn btn-outline-dark">movies</button>
                     </Link>
-                    <Link className="navbar-link" to="/boards">
-                        <button type="button" class="btn btn-outline-success">board games</button>
+                    <Link className="navbar-brand" to="/boards">
+                        <button type="button" class="btn btn-outline-dark">board games</button>
                     </Link>
-                    <Link className="navbar-link" to="/albums">
-                        <button type="button" class="btn btn-outline-success">albums</button>
+                    <Link className="navbar-brand" to="/albums">
+                        <button type="button" class="btn btn-outline-dark">albums</button>
                     </Link>
-                    <Link className="navbar-link" to="/videos">
-                        <button type="button" class="btn btn-outline-success">video games</button>
+                    <Link className="navbar-brand" to="/videos">
+                        <button type="button" class="btn btn-outline-dark">video games</button>
                     </Link>
-                    <Link className="navbar-link" to="/books">
-                        <button type="button" class="btn btn-outline-success">books</button>
+                    <Link className="navbar-brand" to="/books">
+                        <button type="button" class="btn btn-outline-dark">books</button>
                     </Link>
-                    <Link className="navbar-link" to="/television">
-                        <button type="button" class="btn btn-outline-success">television</button>
+                    <Link className="navbar-brand" to="/television">
+                        <button type="button" class="btn btn-outline-dark">television</button>
                     </Link>
-                </div>
+                </nav>
             </div>
         )
     }
