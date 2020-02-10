3883979e95016daee6057b3c347038f6e3afe206
nicholasstano <nicholasstano@gmail.com>
2020-02-10 18:11:18 -0500

    two photos added

--- src/photos/albums/index.js
+++ src/photos/albums/index.js
@@ -1,3 +1,5 @@
 import sgtpeppers from '../albums/sgtpeppers.jpg'
+import thirteensongs from '../albums/thirteensongs.png'
+import shocktroops from '../albums/shocktroops.jpg'
 
-export default { sgtpeppers }
+export default { sgtpeppers, thirteensongs, shocktroops }
