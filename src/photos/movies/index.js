187c74238b027772d1d40573c66ee4719869a272
nicholasstano <nicholasstano@gmail.com>
2020-02-19 09:46:59 -0500

    Added Images, and tweaked a line on the homepage that may have been a type

--- src/photos/movies/index.js
+++ src/photos/movies/index.js
@@ -1,5 +1,6 @@
 import parasite from '../movies/parasite.png'
 import bruteforce from '../movies/bruteforce.jpg'
 import crisscross from '../movies/crisscross.jpg'
+import entourage from '../movies/entourage.jpg'
 
-export default { parasite, bruteforce, crisscross }
+export default { parasite, bruteforce, crisscross, entourage }
