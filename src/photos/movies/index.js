83b253f0110a364e98fb521785715c9da8217efa
nicholasstano <nicholasstano@gmail.com>
2020-02-20 17:20:26 -0500

    New Photo

--- src/photos/movies/index.js
+++ src/photos/movies/index.js
@@ -2,5 +2,6 @@ import parasite from '../movies/parasite.png'
 import bruteforce from '../movies/bruteforce.jpg'
 import crisscross from '../movies/crisscross.jpg'
 import entourage from '../movies/entourage.jpg'
+import birdsofprey from '../movies/birdsofprey.jpg'
 
-export default { parasite, bruteforce, crisscross, entourage }
+export default { parasite, bruteforce, crisscross, entourage, birdsofprey }
