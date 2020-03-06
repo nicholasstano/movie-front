ff82d413f1e5980f448fac038b8e4d12f5fa07a3
nicholasstano <nicholasstano@gmail.com>
2020-03-06 13:21:17 -0500

    Added Photos

--- src/photos/movies/index.js
+++ src/photos/movies/index.js
@@ -6,5 +6,6 @@ import birdsofprey from '../movies/birdsofprey.jpg'
 import whiteheat from '../movies/whiteheat.jpg'
 import mildredpierce from '../movies/mildredpierce.jpg'
 import thetreasureofthesierramadre from '../movies/thetreasureofthesierramadre.jpg'
+import outofthepast from '../movies/outofthepast.jpg'
 
-export default { parasite, bruteforce, crisscross, entourage, birdsofprey, whiteheat, mildredpierce, thetreasureofthesierramadre }
+export default { parasite, bruteforce, crisscross, entourage, birdsofprey, whiteheat, mildredpierce, thetreasureofthesierramadre, outofthepast }
--- src/photos/tvshows/index.js
+++ src/photos/tvshows/index.js
@@ -2,5 +2,7 @@ import entourage5 from '../tvshows/entourage5.jpg'
 import entourage6 from '../tvshows/entourage6.jpg'
 import entourage7 from '../tvshows/entourage7.jpeg'
 import entourage8 from '../tvshows/entourage8.jpg'
+import flightoftheconchords1 from '../tvshows/flightoftheconchords1.jpg'
 
-export default { entourage5, entourage6, entourage7, entourage8 }
+
+export default { entourage5, entourage6, entourage7, entourage8, flightoftheconchords1 }
