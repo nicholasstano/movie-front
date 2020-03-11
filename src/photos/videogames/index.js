4a0dfd32cc6efd5607ff787d282b2a3ad544a781
nicholasstano <nicholasstano@gmail.com>
2020-03-11 11:49:30 -0400

    Added photo

--- src/photos/videogames/index.js
+++ src/photos/videogames/index.js
@@ -1,4 +1,5 @@
 import nhl94 from '../videogames/nhl94.jpg'
 import warcraft3reforged from '../videogames/warcraft3reforged.png'
+import newsupermariobrosudeluxe from '../videogames/newsupermariobrosudeluxe.jpg'
 
-export default { nhl94, warcraft3reforged }
+export default { nhl94, warcraft3reforged, newsupermariobrosudeluxe }
