fcd9aed7a8a442a49e3d74f5cacc467f2ebb33f7
nicholasstano <nicholasstano@gmail.com>
2020-03-30 09:58:40 -0400

    Starting to refactor css on cards

--- src/components/cards/AlbumReviewCard.js
+++ src/components/cards/AlbumReviewCard.js
@@ -1,25 +1,39 @@
 import albumPhotos from '../../photos/albums'
 import React from 'react'
 import StarRateIcon from '@material-ui/icons/StarRate';
+import { Media } from 'reactstrap'
+import albums from '../../photos/albums';
 
 export default function AlbumReviewCard(props) {
     function stars() {
         let stars = []
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < props.album.rating[0]; i++) {
             stars.push(i)
         }
         return stars
     }
     return (
+        // <Media>
+        //     <Media>
+        //         <Media className="cardImage" object src={albumPhotos[props.album.image]} alt={props.album.name} />
+        //     </Media>
+        //     <Media body>
+        //         <Media heading>Media heading</Media>
+        //         <Media heading>Media heading</Media>
+        //         <Media heading>Media heading</Media>
+        //         <Media heading>Media heading</Media>
+
+        //   Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        // </Media>
+        // </Media>
         < div className="mediaReviewCard" key={props.album.id} >
-            <div className="mediaInfoAndPicture">
-                <div>
-                    <img src={albumPhotos[props.album.image]} alt={props.album.name} />
-                </div>
+            <div>
                 <div className="mediaStarsAndDate">
-                    <p>{props.album.month_day_played}/{props.album.year_played}</p>
-                    <p>{props.album.name} ({props.album.year}) by {props.album.artist}</p>
-                    <p>Rating: {stars().map((s, i) => <StarRateIcon key={i} />)}</p>
+                    <Media heading>{props.album.name} ({props.album.year}) by {props.album.artist}</Media>
+                    <Media heading>{props.album.month_day_played}/{props.album.year_played} - Rating: {stars().map((s, i) => <StarRateIcon key={i} />)}</Media>
+                </div>
+                <div className="mediaPicture">
+                    <img src={albumPhotos[props.album.image]} alt={props.album.name} />
                 </div>
             </div>
             <div>
