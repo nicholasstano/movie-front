d932877c4bd438034c3faff98249b2c7d3eee83e
nicholasstano <nicholasstano@gmail.com>
2020-04-27 11:34:19 -0400

    resizing on HomePage for media containers is resizing my favorite 5 media.

--- src/components/mediahomes/AlbumHome.js
+++ src/components/mediahomes/AlbumHome.js
@@ -3,7 +3,7 @@ import HomeTemplate from './HomeTemplate'
 
 export default function AlbumHome() {
 
-    const mediaNames = ["Computerwelt by Kraftwerk", "Safe as Milk by Captain Beefheart", "Arthur by The Kinks", "The Lamb Lies Down on Broadway by Genesis", "Hot Rats by Frank Zappa"]
+    const mediaNames = ["Computerwelt", "Safe as Milk", "Arthur", "The Lamb Lies Down on Broadway", "Hot Rats"]
 
     const mediaIntro = "Chris Jericho defines a perfect album as one where every song is an A or better. I believe a perfect album is an album that you throw on and have a desire to skip no tracks. I guess that's the same thing."
 
--- src/components/mediahomes/HomeTemplate.js
+++ src/components/mediahomes/HomeTemplate.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import './home.css'
+import './home.scss'
 
 export default function HomeTemplate(topFiveMediaNames, photoName, title, paragraphInformation) {
     let favoriteFive = photoName.map((media, index) =>
         <div className="mediaFavoriteFiveSingle" key={media}>
-            <h1>#{index + 1}</h1>
+            <p>{topFiveMediaNames[index]}</p>
             <img src={media} alt={media} />
-            <h4>{topFiveMediaNames[index]}</h4>
         </div>)
     return (
         <div className="mediaHome">
-            <div>
+            <div className="headerInfo">
                 <h1 className="mediaHeader">Favorite {title}</h1>
                 <p>{paragraphInformation}</p>
             </div>
-            {favoriteFive}
+            <div className="favoriteFive">
+                {favoriteFive}
+            </div>
         </div>
     )
 }
