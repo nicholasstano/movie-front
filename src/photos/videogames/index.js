a24c04065bc33e90ec89a6063001a5547d3c508b
nicholasstano <nicholasstano@gmail.com>
2020-05-04 09:53:21 -0400

    Added Review

--- src/photos/videogames/index.js
+++ src/photos/videogames/index.js
@@ -2,5 +2,6 @@ import nhl94 from './nhl94.jpg'
 import warcraft3reforged from './warcraft3reforged.png'
 import newsupermariobrosudeluxe from './newsupermariobrosudeluxe.jpg'
 import supermarioworld from './supermarioworld.png'
+import breathoffireii from './breathoffireii.jpg'
 
-export default { nhl94, warcraft3reforged, newsupermariobrosudeluxe, supermarioworld }
+export default { nhl94, warcraft3reforged, newsupermariobrosudeluxe, supermarioworld, breathoffireii }
