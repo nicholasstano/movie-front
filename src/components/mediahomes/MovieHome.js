0ba49ce27f60801eca0f78044ef7f8faa140374f
nicholasstano <nicholasstano@gmail.com>
2020-05-19 13:42:04 -0400

    More LinkedIn Tests

--- src/components/mediahomes/MovieHome.js
+++ src/components/mediahomes/MovieHome.js
@@ -5,7 +5,7 @@ export default function MovieHome() {
 
     const mediaNames = ["Pulp Fiction", "Taxi Driver", "Chinatown", "The Godfather", "The Usual Suspects"]
 
-    const mediaIntro = "1960's rewatch watchlist: A Shot in the Dark, Sanjuro, A Fistful of Dollars, Dr. Strangelove, For a Few Dollars More, The Sound of Music, The Good The Bad and The Ugly, Once Upon a Time in the West, Butch Cassidy and the Sundance Kid"
+    const mediaIntro = "1960's rewatch watchlist: Sanjuro, A Shot in the Dark, A Fistful of Dollars, Dr. Strangelove, For a Few Dollars More, The Sound of Music, The Good The Bad and The Ugly, Once Upon a Time in the West, Butch Cassidy and the Sundance Kid"
 
     return (
         HomeTemplate(mediaNames, photos.movies, "Movies", mediaIntro)
