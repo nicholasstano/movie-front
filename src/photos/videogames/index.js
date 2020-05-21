d38f5093543315c20b1e778bdb0dcca8abe6f723
nicholasstano <nicholasstano@gmail.com>
2020-05-21 19:27:34 -0400

    Added Review

--- src/photos/videogames/index.js
+++ src/photos/videogames/index.js
@@ -3,5 +3,6 @@ import warcraft3reforged from './warcraft3reforged.png'
 import newsupermariobrosudeluxe from './newsupermariobrosudeluxe.jpg'
 import supermarioworld from './supermarioworld.png'
 import breathoffireii from './breathoffireii.jpg'
+import baldursgate from './baldursgate.png'
 
-export default { nhl94, warcraft3reforged, newsupermariobrosudeluxe, supermarioworld, breathoffireii }
+export default { nhl94, warcraft3reforged, newsupermariobrosudeluxe, supermarioworld, breathoffireii, baldursgate }
