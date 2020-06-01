3380f4c94c4465463e48a9f621e14b954191935c
nicholasstano <nicholasstano@gmail.com>
2020-06-01 08:18:43 -0400

    Added New Text to Movie Home

--- src/components/mediahomes/MovieHome.js
+++ src/components/mediahomes/MovieHome.js
@@ -5,7 +5,7 @@ export default function MovieHome() {
 
     const mediaNames = ["Pulp Fiction", "Taxi Driver", "Chinatown", "The Godfather", "The Usual Suspects"]
 
-    const mediaIntro = "1960's rewatch watchlist: A Shot in the Dark, Sanjuro, A Fistful of Dollars, Dr. Strangelove, For a Few Dollars More, The Sound of Music, The Good The Bad and The Ugly, Once Upon a Time in the West, Butch Cassidy and the Sundance Kid"
+    const mediaIntro = "1960's rewatch watchlist: Sanjuro, Dr. Strangelove, The Sound of Music, The Good The Bad and The Ugly, Once Upon a Time in the West, Butch Cassidy and the Sundance Kid. 1970's rewatch watchlist: Willy Wonka & The Chocolate Factory, The French Connection, The Godfather, Mean Streets, The Sting, Chinatown, The Godfather: Part II, Young Frankenstein, One Flew Over the Cuckoo's Nest, Jaws, Taxi Driver, Star Wars, Halloween, Alien"
 
     return (
         HomeTemplate(mediaNames, photos.movies, "Movies", mediaIntro)
