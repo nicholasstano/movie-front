4b4d96509056a59be2438492ff332ad7193c9647
nicholasstano <nicholasstano@gmail.com>
2020-07-06 23:20:34 -0400

    Updating Colors for the site

--- src/components/homeComponents/AboutMe.js
+++ src/components/homeComponents/AboutMe.js
@@ -3,9 +3,15 @@ import React from 'react'
 export default function AboutMe() {
     return (
         <div className="aboutMe">
-            <h1>Onats Media</h1>
-            <p>Hi! I am Nick and I enjoy (re)visiting media and leaving reviews! I would love to chat if we have similar media interests, I'm always curious to hear about other people and their love for media.</p>
-            <p>I am currently open to new opportunities in Full Stack Development. My contact information is below.</p>
+            <div>
+                <p>Hi! I am Nick and I am a Full Stack Developer currently working on the site <a href="https://www.howlforchange.com" target="_blank">Howl For Change</a>. I began learning JavaScript on my own and attended Flatiron School's Software Engineering Program. I love learning and know certain technologies but wish I knew even more. I am currently open to new opportunities in Full Stack Development.</p>
+            </div>
+            <div>
+                <p>I spent 8.5 years teaching Mathematics in NYC Charter Schools. I currently live in NY with my fiance and our two cats.</p>
+            </div>
+            <div>
+                <p>Welcome to my site where I write reviews when I consume the following media albums, board games, books, movies, tv shows, and video games. One day I may look back and see what I thought of certain things five years ago and see if my opinion has changed. If you want to talk about any media you love or think I would love let's do it.</p>
+            </div>
         </div>
     )
 }
