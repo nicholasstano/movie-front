dc1e0b0772ccd3e57432cb8008ffcc0e24e28c0f
nicholasstano <nicholasstano@gmail.com>
2020-07-11 12:45:48 -0400

    Added Review and updated colors

--- src/photos/boardgames/index.js
+++ src/photos/boardgames/index.js
@@ -3,5 +3,6 @@ import tapestry from './tapestry.jpg'
 import castlesofburgundy from './castlesofburgundy.jpg'
 import kingdombuilder from './kingdombuilder.jpg'
 import kingdomino from './kingdomino.png'
+import paperback from './paperback.jpg'
 
-export default { concordia, tapestry, castlesofburgundy, kingdombuilder, kingdomino }
+export default { concordia, tapestry, castlesofburgundy, kingdombuilder, kingdomino, paperback }
