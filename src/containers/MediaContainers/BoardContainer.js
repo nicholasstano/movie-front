2c2c0caa148ce60664d92a2e8ec256b84835eacc
nicholasstano <nicholasstano@gmail.com>
2020-07-22 10:52:47 -0400

    using class component with Redux for the time being

--- src/containers/MediaContainers/BoardContainer.js
+++ src/containers/MediaContainers/BoardContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import BoardGameHome from '../../components/mediahomes/BoardGameHome.js'
 import BoardGameSidebar from '../../components/sidebars/BoardGameSidebar.js'
 import BoardGameReviewCard from '../../components/cards/BoardGameReviewCard.js'
@@ -6,29 +6,63 @@ import { getBoardGames } from '../../components/actions/boardGameActions'
 import { connect } from 'react-redux'
 import './container.scss'
 
-const BoardContainer = (props) => {
-    const [media, setMedia] = useState(null)
+export class BoardContainer extends Component {
 
-    useEffect(() => {
-        props.getBoardGames()
-    }, [])
+    state = { media: null }
 
-    const mediaClickHandler = (mediaClicked) => {
-        window.scrollTo(0, 0)
-        setMedia(mediaClicked)
+    componentDidMount() {
+        this.props.getBoardGames()
     }
-    console.log(props)
-    return (
-        <div className="mediaContainer">
-            <div>
-                <BoardGameSidebar reviews={props.boardGames.boardGames} mediaClickHandler={mediaClickHandler} />
+
+    mediaClickHandler = (mediaClicked) => {
+        window.scrollTo(0, 0)
+        this.setState({ media: mediaClicked })
+    }
+
+    render() {
+        const { boardGames } = this.props.boardGames
+        const { media } = this.state
+        return (
+            <div className="mediaContainer">
+                <div>
+                    <BoardGameSidebar reviews={boardGames} mediaClickHandler={this.mediaClickHandler} />
+                </div>
+                <div className="mediaContent">
+                    {media ? <BoardGameReviewCard key={media.id} board={media} /> : <BoardGameHome />}
+                </div>
             </div>
-            <div className="mediaContent">
-                {media ? <BoardGameReviewCard key={media.id} board={media} /> : <BoardGameHome />}
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
+// const BoardContainer = (props) => {
+//     const [media, setMedia] = useState(null)
+//     // const [boardGames, setBoardGames] = useState([])
+//     const boardGames = props.boardGames.boardGames
+//     const getBoardGames = props.getBoardGames
+
+//     useEffect(() => {
+//         getBoardGames()
+//         // props.getBoardGames()
+//         // setBoardGames(props.boardGames.boardGames)
+//     }, [])
+
+//     const mediaClickHandler = (mediaClicked) => {
+//         window.scrollTo(0, 0)
+//         setMedia(mediaClicked)
+//     }
+//     console.log(props.boardGames.boardGames)
+//     return (
+//         <div className="mediaContainer">
+//             <div>
+//                 <BoardGameSidebar reviews={boardGames} mediaClickHandler={mediaClickHandler} />
+//             </div>
+//             <div className="mediaContent">
+//                 {media ? <BoardGameReviewCard key={media.id} board={media} /> : <BoardGameHome />}
+//             </div>
+//         </div>
+//     )
+// }
 
 const mapStateToProps = state => ({
     boardGames: state.boardGames
