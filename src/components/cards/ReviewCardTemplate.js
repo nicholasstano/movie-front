ddd3dc3ef302221e3f3c1fe0957ce36bac3bb180
nicholasstano <nicholasstano@gmail.com>
2020-08-03 11:28:53 -0400

    Refactored Star Ratings for ReviewS

--- src/components/cards/ReviewCardTemplate.js
+++ src/components/cards/ReviewCardTemplate.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import StarRateIcon from '@material-ui/icons/StarRate';
+import StarRate from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import './reviewcard.scss'
 
 export default function ReviewCardTemplate(name, photos, rating, image, monthDayConsumed, yearConsumed, notes, improveNotes) {
 
     function stars() {
-        let stars = []
+        let starArr = []
         for (let i = 0; i < rating[0]; i++) {
-            stars.push(i)
+            starArr.push(1)
         }
-        return stars
+        while (starArr.length !== 5) {
+            starArr.push(0)
+        }
+        return starArr
     }
 
     return (
@@ -18,7 +22,7 @@ export default function ReviewCardTemplate(name, photos, rating, image, monthDay
                 <p className="mediaPicture">
                     <img src={photos[image]} alt={name} />
                 </p>
-                <p>Rating: {stars().map((s, i) => <StarRateIcon key={i} />)}</p>
+                <p>Rating: {stars().map((s, i) => s === 1 ? <StarRate key={i} /> : <StarBorderIcon key={i} />)}</p>
                 <p>Date: {monthDayConsumed}/{yearConsumed}</p>
             </div>
             <h5 className='goodHeader'>The Good</h5>
