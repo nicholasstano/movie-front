8d8702c6f1ce4c40396096cc0d8499703764f4fa
nicholasstano <nicholasstano@gmail.com>
2020-08-27 17:32:54 -0400

    New Review

--- src/photos/tvshows/index.js
+++ src/photos/tvshows/index.js
@@ -16,5 +16,6 @@ import thebestofwhoselineisitanyway from './thebestofwhoselineisitanyway.jpg'
 import whoselineisitanyway1 from './whoselineisitanyway1.jpeg'
 import whoselineisitanyway2 from './whoselineisitanyway2.jpeg'
 import entourage1 from './entourage1.jpg'
+import whoselineisitanyway14 from './whoselineisitanyway14.jpeg'
 
-export default { entourage5, entourage6, entourage7, entourage8, flightoftheconchords1, flightoftheconchords2, agentcarter1, dragonballzkai1, dragonballsuper1, dragonballzkai2, dragonballzkai3, shaqlife, themandalorian1, whoselineisitanywayaisha, thebestofwhoselineisitanyway, whoselineisitanyway1, whoselineisitanyway2, entourage1 }
+export default { entourage5, entourage6, entourage7, entourage8, flightoftheconchords1, flightoftheconchords2, agentcarter1, dragonballzkai1, dragonballsuper1, dragonballzkai2, dragonballzkai3, shaqlife, themandalorian1, whoselineisitanywayaisha, thebestofwhoselineisitanyway, whoselineisitanyway1, whoselineisitanyway2, entourage1, whoselineisitanyway14 }
