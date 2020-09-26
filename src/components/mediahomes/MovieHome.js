f5464af52caf8ff78b5f9a84aaa4918bb84cd132
nicholasstano <nicholasstano@gmail.com>
2020-09-26 17:38:30 -0400

    edited movie home

--- src/components/mediahomes/MovieHome.js
+++ src/components/mediahomes/MovieHome.js
@@ -5,7 +5,7 @@ export default function MovieHome() {
 
     const mediaNames = ["Pulp Fiction", "Taxi Driver", "Chinatown", "The Godfather", "The Usual Suspects"]
 
-    const mediaIntro = "-----1970's rewatch watchlist: \n -Star Wars \n -----1980's rewatch watchlist: \n -The Empire Strikes Back \n -Return of the Jedi \n -Raging Bull \n -The Shining \n -The Road Warrior \n -Blade Runner \n -Scarface \n -Once Upon a Time in America \n -Ran \n -Aliens \n -Big Trouble in Little China \n -Blue Velvet \n -Die Hard"
+    const mediaIntro = "-----1970's rewatch watchlist: \n -Star Wars \n -----1980's rewatch watchlist: \n -The Empire Strikes Back \n -Return of the Jedi \n -Raging Bull \n -The Shining \n -The Road Warrior \n -Blade Runner \n -Scarface \n -Once Upon a Time in America \n -Ran \n -Big Trouble in Little China \n -Blue Velvet \n -Die Hard"
 
     return (
         HomeTemplate(mediaNames, photos.movies, "Movies", mediaIntro)
