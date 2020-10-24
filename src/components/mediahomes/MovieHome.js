a55cf572ce2b1752d2354bee28f1dd6705db7219
nicholasstano <nicholasstano@gmail.com>
2020-10-24 11:45:08 -0400

    Added Review

--- src/components/mediahomes/MovieHome.js
+++ src/components/mediahomes/MovieHome.js
@@ -5,7 +5,7 @@ export default function MovieHome() {
 
     const mediaNames = ["Pulp Fiction", "Taxi Driver", "Chinatown", "The Godfather", "The Usual Suspects"]
 
-    const mediaIntro = "-----Rewatch Watchlist: \n -Seven Samurai \n -The Sound of Music \n -Star Wars \n -The Empire Strikes Back \n -Return of the Jedi \n -Raging Bull \n -The Shining \n -Scarface \n -Die Hard \n -Back to the Future Part III"
+    const mediaIntro = "-----Rewatch Watchlist: \n -Seven Samurai \n -The Sound of Music \n -Star Wars \n -The Empire Strikes Back \n -Return of the Jedi \n -Raging Bull \n -The Shining \n -Scarface \n -Back to the Future Part III \n -Glengarry Glen Ross"
 
     return (
         HomeTemplate(mediaNames, photos.movies, "Movies", mediaIntro)
