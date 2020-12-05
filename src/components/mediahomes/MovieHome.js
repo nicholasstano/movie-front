3f38e7c50345bd11e9c4c8a2f5e9904679326e07
nicholasstano <nicholasstano@gmail.com>
2020-12-05 13:37:13 -0500

    Added Review changed watchlist

--- src/components/mediahomes/MovieHome.js
+++ src/components/mediahomes/MovieHome.js
@@ -5,7 +5,7 @@ export default function MovieHome() {
 
     const mediaNames = ["Pulp Fiction", "Taxi Driver", "Chinatown", "The Godfather", "The Usual Suspects"]
 
-    const mediaIntro = "-----Rewatch Watchlist: \n -Seven Samurai \n -The Sound of Music \n -The Empire Strikes Back \n -Return of the Jedi \n -Back to the Future Part III"
+    const mediaIntro = "-----Rewatch Watchlist: \n -Seven Samurai \n -The Sound of Music \n -Return of the Jedi \n -Back to the Future Part III"
 
     return (
         HomeTemplate(mediaNames, photos.movies, "Movies", mediaIntro)
